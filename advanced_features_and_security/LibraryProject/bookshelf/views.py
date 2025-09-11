6684a280a47e4b465caca7e34867b14a98d078f1
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-11 11:41:35 +0000

    Import ExampleForm in views.py for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,9 +3,8 @@
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
 from .models import Book
-from .forms import BookSearchForm  # You can create this form (see below)
+from .forms import BookSearchForm, ExampleForm  # Import both forms
 
-# View to list books, requires user to be logged in and have permission.
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
@@ -21,9 +20,26 @@ def book_list(request):
     if form.is_valid():
         title = form.cleaned_data.get('title')
         if title:
+            # Filter books safely using Django ORM with parameterized queries
             books = books.filter(title__icontains=title)
 
     return render(request, 'bookshelf/book_list.html', {
         'books': books,
         'form': form
     })
+
+@login_required
+def example_form_view(request):
+    """
+    Example form view demonstrating secure form handling with CSRF protection.
+    """
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data here securely
+            # For example, save data or perform actions
+            pass
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
