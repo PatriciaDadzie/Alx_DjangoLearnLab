ba04900f7f41b18708db9178436c57c2961ac2ad
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-23 18:58:00 +0000

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 
