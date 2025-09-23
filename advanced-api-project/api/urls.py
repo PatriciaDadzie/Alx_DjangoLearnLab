75f4cd8695b33041519b8bd071076324014f685c
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-23 16:21:00 +0000

    Added DRF generic views and API routing for Book model

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/update/<int:pk>/', BookDetailView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDetailView.as_view(), name='book-delete'),
 ]
