15bea39e95c959a3222af47406183ff3636e5fb2
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-23 16:34:34 +0000

    Implement filtering, searching, and ordering in BookListView with django-filter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics, mixins, filters
+from rest_framework import generics, mixins, filters as drf_filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters 
+
 from .models import Book
 from .serializers import BookSerializer
+
 
 
 class BookListView(mixins.ListModelMixin,
