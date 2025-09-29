a33d7cb6fa302c77e6a6be0e9042e6da01a3c17e
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-29 10:08:05 +0000

    Initial Django Blog setup with Post model

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
