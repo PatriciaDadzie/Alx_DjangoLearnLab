db0052f47d79856f5fc7f52db0b7e29c7a41fe66
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-30 12:24:00 +0000

    Add URL routes for comment create, edit, delete

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,4 +17,9 @@ urlpatterns = [
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
+
+    # Comment CRUD
+    path("post/<int:pk>/comments/new/", views.add_comment, name="add-comment"),
+    path("comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
+    path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
