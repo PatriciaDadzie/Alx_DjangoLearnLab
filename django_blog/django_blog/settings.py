f0b54cb128abf01ba2814702a56b06fec2b613b6
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-30 04:13:21 +0000

    fixed database configurations from sqlite to prostgres

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_blog_db",      
+        "USER": "postgres",            
+        "PASSWORD": "yourpassword",    
+        "HOST": "localhost",           
+        "PORT": "5432",                
     }
 }
+
 
 
 
