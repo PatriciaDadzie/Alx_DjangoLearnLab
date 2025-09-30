e4d49ee65b2dad4239131400cbfc7584ecf1dbde
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-30 12:22:41 +0000

    Add comment views: add, update, delete; include comment_form in post detail

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm, PostForm
-from .models import Post
+from django.urls import reverse_lazy, reverse
+
+from .forms import (
+    UserRegistrationForm,
+    UserUpdateForm,
+    ProfileUpdateForm,
+    PostForm,
+    CommentForm,   # ✅ import the comment form
+)
+from .models import Post, Comment   # ✅ import Comment model
 
 
+# ----------------------
+# Auth & Profile Views
+# ----------------------
 def register(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
@@ -28,7 +38,9 @@ def register(request):
 def profile(request):
     if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -42,6 +54,9 @@ def profile(request):
     return render(request, "blog/profile.html", {"u_form": u_form, "p_form": p_form})
 
 
+# ----------------------
+# Post Views
+# ----------------------
 class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
@@ -52,6 +67,12 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
+
+    def get_context_data(self, **kwargs):
+        """Add comment form + comments to the post detail context"""
+        context = super().get_context_data(**kwargs)
+        context["comment_form"] = CommentForm()
+        return context
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -86,3 +107,47 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+
+# ----------------------
+# Comment Views
+# ----------------------
+@login_required
+def add_comment(request, pk):
+    """Handle posting a new comment on a post."""
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.success(request, "Your comment has been added.")
+            return redirect("blog:post-detail", pk=post.pk)
+        else:
+            messages.error(request, "There was a problem with your comment.")
+    return redirect("blog:post-detail", pk=post.pk)
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def get_success_url(self):
+        return reverse("blog:post-detail", kwargs={"pk": self.object.post.pk})
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = "blog/comment_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse("blog:post-detail", kwargs={"pk": self.object.post.pk})
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
