bda8772e97a72faad39a0541e22fec8edc479f80
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-02 13:44:36 +0000

    Add search functionality and tag filtering in post views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
+from django.db.models import Q   
+
 
 from .forms import (
     UserRegistrationForm,
@@ -167,3 +169,21 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return self.request.user == self.get_object().author
     
     
+
+# ----------------------
+# Search View
+# ----------------------
+class PostSearchView(ListView):
+    model = Post
+    template_name = "blog/search_results.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            return Post.objects.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+        return Post.objects.none()
