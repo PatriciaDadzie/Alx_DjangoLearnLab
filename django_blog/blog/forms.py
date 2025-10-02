72fd9420b02dd1fe314be22f213582639045dac0
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-02 13:11:32 +0000

    Added Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Post, Comment, Tag
+from .models import Profile, Post, Comment
 
 
 # ----------------------
@@ -30,46 +30,12 @@ class ProfileUpdateForm(forms.ModelForm):
 
 
 # ----------------------
-# Post Form (with tags)
+# Post Form (with django-taggit)
 # ----------------------
 class PostForm(forms.ModelForm):
-    # extra field for comma-separated tags
-    tags_field = forms.CharField(
-        required=False,
-        help_text="Enter comma-separated tags (e.g. django, python, tutorial)",
-        widget=forms.TextInput(attrs={"placeholder": "tag1, tag2, tag3"}),
-    )
-
     class Meta:
         model = Post
-        fields = ["title", "content"]
-
-    def __init__(self, *args, **kwargs):
-        instance = kwargs.get("instance")
-        super().__init__(*args, **kwargs)
-        if instance:
-            # pre-populate with existing tags
-            self.fields["tags_field"].initial = ", ".join(
-                [tag.name for tag in instance.tags.all()]
-            )
-
-    def clean_tags_field(self):
-        raw = self.cleaned_data.get("tags_field", "")
-        tags = [t.strip() for t in raw.split(",") if t.strip()]
-        return tags
-
-    def save(self, commit=True):
-        post = super().save(commit=commit)
-        tags = self.cleaned_data.get("tags_field", [])
-        if commit:
-            # clear and re-attach tags
-            post.tags.clear()
-            for tag_name in tags:
-                tag_obj, _ = Tag.objects.get_or_create(name=tag_name)
-                post.tags.add(tag_obj)
-        else:
-            self._pending_tags = tags
-        return post
+        fields = ["title", "content", "tags"]   # 'tags' comes from taggit
 
 
 # ----------------------
