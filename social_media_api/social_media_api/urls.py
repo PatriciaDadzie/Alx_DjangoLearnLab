152a378a5a94f3b632e08ccc179dbe01871d8279
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-08 14:28:36 +0000

    Add posts router and include posts urls in project urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,11 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# social_media_api/urls.py (snippet)
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
+    path('api/', include('posts.urls')),  # posts routes: /api/posts/ and /api/comments/
 ]
 
-# only for dev: serve media files
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
