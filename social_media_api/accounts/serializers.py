0bc8ee8d109a30d6e147154cb284923e9db300dd
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-08 13:00:58 +0000

    Fix RegisterSerializer to use create_user and explicit CharField for password

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,8 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
 
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
+    password = serializers.CharField(write_only=True, min_length=8)  
 
     class Meta:
         model = User
@@ -18,9 +19,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        # create token
+        user = get_user_model().objects.create_user(password=password, **validated_data)
         Token.objects.create(user=user)
         return user
