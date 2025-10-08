f269f55e971b18358842dcadd8290818da5272d9
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-08 15:14:15 +0000

    Add FeedView to return posts from followed users for authenticated feed feature

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets, permissions, filters
+
+from rest_framework import viewsets, permissions, filters, generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
@@ -37,3 +41,19 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        followed_users = user.following.all()
+        return (
+            Post.objects.filter(author__in=followed_users)
+            .select_related('author')
+            .prefetch_related('comments')
+            .order_by('-created_at')
+        )
