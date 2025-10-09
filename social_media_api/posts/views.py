9fbb72b76e2a27a1d7ef6d3cfae4925016505581
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-09 11:45:07 +0000

    Add Like and Unlike post views with notifications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,42 +1,14 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from .models import Post, Like
-from .serializers import PostSerializer
-from notifications.models import Notification  
+from notifications.models import Notification
 
 
-class FeedView(generics.GenericAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        following_users = user.following.all()
-        posts = Post.objects.filter(author__in=following_users).order_by("-created_at")
-        serializer = self.get_serializer(posts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class CreatePostView(generics.GenericAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    queryset = Post.objects.all()
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# Like and Unlike Views
 class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk, *args, **kwargs):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
@@ -58,7 +30,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk, *args, **kwargs):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
 
         if not like:
