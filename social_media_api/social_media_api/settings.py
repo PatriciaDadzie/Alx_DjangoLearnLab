eb0b7efc4a80c3bcfbd7578cbaaec2f4f61b44e2
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-09 11:25:15 +0000

    Create notifications app and add to INSTALLED_APPS

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'posts',
     'django_filters',
+    'notifications',
 ]
 
 
@@ -144,5 +145,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
