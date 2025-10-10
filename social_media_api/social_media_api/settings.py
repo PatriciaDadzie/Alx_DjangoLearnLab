baa7e22da356cbea190b158097e034254accd5f3
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-10 12:27:46 +0000

    Add dj-database-url and production DB config

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -95,11 +95,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 # -------------------------------------------------------------------
 # DATABASE CONFIGURATION
-# -------------------------------------------------------------------
-# Uses DATABASE_URL from .env (e.g. postgres://user:pass@host:port/dbname)
+
+import dj_database_url
 DATABASES = {
-    'default': env.db(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    "default": dj_database_url.parse(env("DATABASE_URL"), conn_max_age=600)
 }
+
 
 # -------------------------------------------------------------------
 # PASSWORD VALIDATION
