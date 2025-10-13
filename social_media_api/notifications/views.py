f40ed9f1dd4fc366bb12542d37d46e930c70a3c5
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-13 09:22:05 +0000

    Add FeedView to generate feed from followed users ordered by creation date

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,4 +1,9 @@
 from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.response import Response
+from rest_framework import permissions, status
+
 
 # ---------------------------------------------------------------
 # FEED VIEW
